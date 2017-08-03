2e13968fff2d71ca4090b98542aff61fb85b89f2
Christopher Thomson <christopher.thomson42@gmail.com>
2017-08-03 12:42:55 -0700

    Sorry I cloned from Marty's Repo not my own

---
+++ tests/unit/user.test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const User = require('../../lib/models/user');
+
+describe('user model', () => {
+    it('new user generates hash', () => {
+        const user = new User({
+            email: 'tsttxt@woo.com'
+        });
+        const password = 'abc';
+        user.generateHash(password);
+
+        assert.notEqual(user.hash, password);
+        assert.isOk(user.comparePassword('abc'));
+        assert.isNotOk(user.comparePassword('bad password'));
+    });
+});
